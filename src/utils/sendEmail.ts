0f39d23b6a7d8cfec7b8871d662f72f88eafe0ee
Sabithabasima <sabithabasimavk@gmail.com>
2026-05-07 14:29:15 +0530

    to resolve the otp mailer issue in live

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,14 +1,36 @@
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 import { env } from "../config/env";
 
-export const sendEmail = async (to: string, subject: string, html: string) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
+export const sendEmail = async (
+  to: string,
+  subject: string,
+  html: string
+) => {
+  const config: SMTPTransport.Options = {
+    host: "smtp.gmail.com",
+
+    port: 587,
+
+    secure: false,
+
     auth: {
       user: env.EMAIL_USER,
       pass: env.EMAIL_PASS,
     },
-  });
+
+    tls: {
+      rejectUnauthorized: false,
+    },
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+  };
+
+  const transporter = nodemailer.createTransport(config);
+
+  await transporter.verify();
 
   await transporter.sendMail({
     from: `"Fazzmi" <${env.EMAIL_USER}>`,
